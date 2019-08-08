5c5a8afef1a4abb9d843658a279e855da3e0fcd8
Aatharsh Girishwar <vijayaaath@ecs.vuw.ac.nz>
2019-08-08 16:45:23 +1200

    Populated Nav-bar with backend data

--- code/client/src/components/MapNav/MapNav.js
+++ code/client/src/components/MapNav/MapNav.js
@@ -7,146 +7,148 @@ import { getTravelData, getDemographicData, getCities, getTime } from "../../uti
 import "./MapNav.css";
 
 import {
-    // componentDidMount,
-    // componentDidUpdate,
-    // handleDropDownChange,
-    // resetState,
-    // mapStateToProps,
-    // getValidOptions,
-    // dropdown
-    // addQuickInfoBox
+  // componentDidMount,
+  // componentDidUpdate,
+  // handleDropDownChange,
+  // resetState,
+  // mapStateToProps,
+  // getValidOptions,
+  // dropdown
+  // addQuickInfoBox
 } from './NavBarHelpers';
 
 
 class MapNav extends Component {
-    state = {
-        city: 'city',
-        to: 'to',
-        from: 'from',
-        location: 'area'
-    };
+  state = {
+    city: 'city',
+    to: 'to',
+    from: 'from',
+    location: 'area',
+    demographic: [],
+    travel: [],
+    cities: [],
+    times: []
+  };
 
-    data = {
-        demographic: [],
-        travel: [],
-        cities: [],
-        times: []
-    }
+  // set up and bind dropdown from NavBarHelpers
+  constructor(props) {
+    super(props);
+    // this.dropdown = dropdown.bind(this);
+  }
 
+  componentDidMount() {
+    this.getData();
+  }
 
-    // set up and bind dropdown from NavBarHelpers
-    constructor(props) {
-        super(props);
-        // this.dropdown = dropdown.bind(this);
-    }
-
-    componentWillMount() {
-        this.getData();
-    }
-
-    getData(){
-        getTravelData("travel", localStorage.getItem('auth'))
-        .then(
-            data => {
-                this.data.travel = data;
-            });
-
-        getDemographicData("geocode", localStorage.getItem('auth'))
-          .then(
-            data => {
-                this.data.demographic = data;
-        });
-
-        getCities("cities", localStorage.getItem('auth'))
-            .then(
-                data => {
-                    this.data.cities = data;
-        })
-            
-
-
-        getTime("time", localStorage.getItem('auth'))
-            .then(data => {
-                this.data.times = data;
-        })
-
-        console.log(this.data)
-        }
-    
-
-    handleChange = (event) => {
-        this.setState({
+  getData() {
+    getTravelData("travel", localStorage.getItem('auth'))
+      .then(
+        data => {
 
         });
-    }
 
-    useStyles = makeStyles(theme => ({
-        root: {
-          flexGrow: 1,
-        },
-        paper: {
-          padding: theme.spacing(2),
-          textAlign: 'center',
-          color: theme.palette.text.secondary,
-        },
-      }));
+    getDemographicData("geocode", localStorage.getItem('auth'))
+      .then(
+        data => {
+        });
+
+    getCities("cities", localStorage.getItem('auth'))
+      .then(
+        data => {
+          this.setState({ cities: data });
+          console.log(this.state.cities)
+        })
+
+
+
+    getTime("time", localStorage.getItem('auth'))
+      .then(data => {
+        this.setState({ times: data });
+        console.log(this.state.times)
+      })
+  }
+
+
+  handleCityChange = (event) => {
+    console.log("EVENT VAL : " + event.target.value)
+    this.setState({city : event.target.value})
+  }
+
+  handleChange = (event) => {
+    console.log("EVENT VAL : " + event.target.value)
+    if(event === 'city') {
+    console.log("City selected ")
+    }
+  }
+
+  useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 
   render() {
-      // render a grid that holds dropdown menu options
+    // render a grid that holds dropdown menu options
     return (
-        <div className={this.useStyles.root}>
+      <div className={this.useStyles.root}>
         <Grid container spacing={2}>
-            <Grid item xs={3}>
-                <FormControl>
-                    <InputLabel className={this.useStyles.paper}>City</InputLabel>
-                    <Select
-                        onChange={this.handleChange}>
-                        <MenuItem value={this.state.city}>
-                            {console.log(this.data)}
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
+          <Grid item xs={3}>
+            <FormControl>
+              <InputLabel className={this.useStyles.paper}>{this.state.city}</InputLabel>
+              <Select
+                onChange={this.handleCityChange}>
 
-            <Grid item xs={3}>
-                <FormControl className="NavForm">
-                    <InputLabel className={this.useStyles.paper}>From</InputLabel>
-                    <Select
-                        onChange={this.handleChange}>
-                        <MenuItem value={this.state.from}>
-                            {this.data.times}
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
+                {this.state.cities.map(city => {
+                  return <MenuItem value={city}> {city} </MenuItem>
+                })}
 
-            <Grid item xs={3}>
-                <FormControl>
-                    <InputLabel>To</InputLabel>
-                    <Select
-                        onChange={this.handleChange}>
-                        <MenuItem value={this.state.to}>
-                        {this.data.times}
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
+              </Select>
+            </FormControl>
+          </Grid>
 
-            <Grid item xs={3}>
-                <FormControl>
-                    <InputLabel>Time</InputLabel>
-                    <Select
-                        onChange={this.handleChange}>
-                        <MenuItem value={this.state.location}>
-                            {this.state.location}
-                        </MenuItem>
-                    </Select>
-                </FormControl>                
-            </Grid>
+          <Grid item xs={3}>
+            <FormControl className="NavForm">
+              <InputLabel className={this.useStyles.paper}>From</InputLabel>
+              <Select
+                onChange={this.handleChange}>
+                {this.state.times.map(time => {
+                  return <MenuItem value={time}> {time} </MenuItem>
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={3}>
+            <FormControl>
+              <InputLabel>To</InputLabel>
+              <Select
+                onChange={this.handleChange}>
+                {this.state.times.map(time => {
+                  return <MenuItem value={time}> {time} </MenuItem>
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={3}>
+            <FormControl>
+              <InputLabel>Location</InputLabel>
+              <Select
+                onChange={this.handleChange}>
+                <MenuItem value={this.state.location}>
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
 
-    </div>
+      </div>
     );
   }
 }
