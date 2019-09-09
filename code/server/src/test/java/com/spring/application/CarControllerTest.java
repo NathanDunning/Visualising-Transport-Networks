7e842205d46db62a3282806bde961ee258090f58
vijayaaath <vijayaaath@ecs.vuw.ac.nz>
2019-09-09 17:19:43 +1200

    Added backend unit test cases - Mob programmed

--- code/server/src/test/java/com/spring/application/CarControllerTest.java
+++ code/server/src/test/java/com/spring/application/CarControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,6 +29,7 @@ import com.spring.application.service.CarService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class CarControllerTest {
 
 	@Autowired
