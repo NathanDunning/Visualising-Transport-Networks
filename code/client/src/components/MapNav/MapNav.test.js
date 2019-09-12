88c21f104750fd6ade7b37160dc88a01b43c9805
tamdori <tamdori138@gmail.com>
2019-09-12 15:03:25 +1200

    MapNav test passes with comments to explain the test

--- code/client/src/components/MapNav/MapNav.test.js
+++ code/client/src/components/MapNav/MapNav.test.js
@@ -2,22 +2,26 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react'
 
-
 import MapNav from './MapNav';
-import { GridListTileBar } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid/';
+import {
+    FormControl, InputLabel, MenuItem, Select,
+} from '@material-ui/core';
 
-configure({ adapter: new Adapter()});
+
+configure({ adapter: new Adapter() });
 
 // The name inside < /> is what you'll see in console.output
 describe('<MapNav />', () => {
 
     // write tests in here
 
-    
-    it('should render one <MapNav /> element', () => {
-        const wrapper = shallow(<MapNav />);   
-        expect(wrapper.find(<Grid/>));
+    it('should render one <Grid /> element', () => {
+        // wrapper is a container for where to test a specific component
+        const wrapper = shallow(<MapNav />);
+        // there are 4 FormControl elements in the MapNav.js, so we check that there are four of those elements
+        expect(wrapper.find(FormControl)).toHaveLength(4);
     });
 });
 
-// Enzyme allows us to render our components to be standalone, isolated. shallow renders react components
+// Enzyme allows us to render our components to be standalone, isolated/unit tests, and independent of the rest of the application. shallow renders react components.
